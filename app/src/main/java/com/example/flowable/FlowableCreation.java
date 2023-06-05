ff12cb2b47adba05af60fdbbd9ef684fe5548148
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-06-05 14:33:58 +0530

    Flowable Creation

---
+++ app/src/main/java/com/example/flowable/FlowableCreation.java
@@ -0,0 +1,37 @@
+package com.example.flowable;
+
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+//output
+
+//0
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+
+public class FlowableCreation {
+    public static void main(String [] args) throws InterruptedException{
+        Flowable.create(emitter -> {
+            for (int i = 0 ;i< 10; i++){
+                if(emitter.isCancelled()){
+                    return;
+                }
+                emitter.onNext(i);
+            }
+                emitter.onComplete();
+                },
+                BackpressureStrategy.BUFFER).
+                  observeOn(Schedulers.io())
+                .subscribe(System.out::println);
+
+        Thread.sleep(2000);
+    }
+}
